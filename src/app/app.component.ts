bb4f03a3e486ed04bd6e10895d2736f79f49d22f
Ketan Mehta <meetketanmehta@gmail.com>
2022-07-15 02:40:08 +0530

    reactive grouping control ,  starting using form grouping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent implements OnInit{
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
   signupForm: FormGroup;
-  ngOnInit(){
+  ngOnInit() {
     this.signupForm = new FormGroup({
-      'username': new FormControl(null,Validators.required),
-      'email':new FormControl(null,[Validators.required,Validators.email]),
-      'gender':new FormControl('male')
-    })
+      userData: new FormGroup({
+        username: new FormControl(null, Validators.required),
+        email: new FormControl(null, [Validators.required, Validators.email]),
+      }),
+      gender: new FormControl("male"),
+    });
   }
 }
