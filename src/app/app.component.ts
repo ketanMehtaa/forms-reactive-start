cf0d1c0873c002e87e91ff6a5ba32b9597f12c8d
Ketan Mehta <meetketanmehta@gmail.com>
2022-07-16 19:39:51 +0530

    custom validators dont forgot to this.validator.bind(this)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,11 @@ import { FormArray, FormControl, FormGroup, Validators } from "@angular/forms";
 export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupForm: FormGroup;
+  noUser = ["ketan", "manjeet"];
   ngOnInit() {
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [Validators.required,this.forbiddenNames.bind(this)]),
         email: new FormControl(null, [Validators.required, Validators.email]),
       }),
       gender: new FormControl("male"),
@@ -23,13 +24,23 @@ export class AppComponent implements OnInit {
   //   return (this.signupForm.get('hobbies') as FormArray).controls;
   // }
   getControls() {
-    return (<FormArray>this.signupForm.get('hobbies')).controls;
+    return (<FormArray>this.signupForm.get("hobbies")).controls;
   }
-  onAddHobby(){
-    const control = new FormControl(null,Validators.required);
-    (<FormArray>this.signupForm.get('hobbies')).push(control);
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
   }
-  onSubmit(){
+  onSubmit() {
     console.log(this.signupForm);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.noUser.indexOf(control.value) !== -1) {
+      // console.log(this.noUser.indexOf(control.value));
+      return { nameIsForbidden: true };
+    }
+    // console.log(this.noUser.indexOf(control.value));
+
+    return null;
   }
 }
